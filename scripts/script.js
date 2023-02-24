a3fc7e11535caed8c8fe8a13ae656a8a068ac34f
Marcos Costa Pinheiro <marquinhos_pinheiro14@outlook.com>
2023-02-23 21:14:01 -0300

    criei o menu de categorias

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,19 @@ var input = document.getElementById('search-box')
 var label = document.getElementById('ilabel')
 var menu = document.getElementById('icon-menu')
 var options = document.getElementById('options')
+var titleCategories = document.getElementById('title-categories')
+var categories = document.getElementById('box-categories')
+var arrow = document.getElementById('arrow')
 
 function reveal(clicked, target){
     if(visibility){
-        target.style.display = 'none'
-        clicked.style.backgroundColor = 'transparent'
-        clicked.style.color = 'white'
-        clicked.style.borderRadius = '0'
-        visibility = false
+        if(clicked === label || clicked === menu){
+            target.style.display = 'none'
+            clicked.style.backgroundColor = 'transparent'
+            clicked.style.color = 'white'
+            clicked.style.borderRadius = '0'
+            visibility = false
+        }
         if(clicked === menu){
             clicked.style.transform = 'rotate(0)'
             clicked.style.padding = '5px 10px'
@@ -18,36 +23,59 @@ function reveal(clicked, target){
         if(clicked === label){
             label.innerText = 'search'
         }
+        if(clicked === titleCategories){
+            clicked.style.backgroundColor = 'transparent'
+            target.style.display = 'none'
+            arrow.innerText = 'chevron_right'
+            visibility = false
+        }
     }
     else{
-        clicked.style.backgroundColor = 'transparent'
-        clicked.style.color = 'black'
-        clicked.style.backgroundColor = '#31465f'
-        target.style.display = 'block'
-        visibility = true
+        if(clicked === label || clicked === menu){
+            clicked.style.backgroundColor = 'transparent'
+            clicked.style.color = 'black'
+            clicked.style.backgroundColor = '#31465f'
+            target.style.display = 'block'
+            visibility = true
+        }
+        
         if(clicked === menu){
             clicked.style.transform = 'rotate(270deg)'
         }
         if(clicked === label){
             label.innerText = 'search_off'
         }
+        if(clicked === titleCategories){
+            clicked.style.backgroundColor = '#465f7a'
+            target.style.display = 'block'
+            arrow.innerText = 'expand_more'
+            visibility = true
+        }
     }
 }
 
 function mouseEnter(target){
     if(visibility === false){
-        target.style.backgroundColor = 'white'
-        target.style.cursor = 'pointer'
-        target.style.color = 'black'
-        target.style.borderRadius = '5px'
+        if(target === label || target == menu){
+            target.style.backgroundColor = 'white'
+            target.style.cursor = 'pointer'
+            target.style.color = 'black'
+            target.style.borderRadius = '5px'
+        }
+        if(target === titleCategories){
+            target.style.cursor = 'pointer'
+        }
     }
     
 }
 
 function mouseOut(target){
     if(visibility === false){
-        target.style.backgroundColor = 'transparent'
-        target.style.color = 'white'
-        target.style.borderRadius = '0'
+        if(target === label || target == menu){
+            target.style.backgroundColor = 'transparent'
+            target.style.color = 'white'
+            target.style.borderRadius = '0'
+        }
+        
     }
 }
